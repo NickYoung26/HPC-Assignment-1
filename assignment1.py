eb356e4491e64d2489d0a1ce6b1319df39d17532
Nicholas Young <nicholas.young.2021@uni.strath.ac.uk>
2026-02-02 22:22:47 +0000

    Update assignment1.py
    First time using github, so changes have been copied and pasted from testing and editing on thinlinc.

--- assignment1.py
+++ assignment1.py
@@ -1,64 +1,35 @@
-#!/usr/bin/env python3
+#!/opt/software/anaconda/python-3.10.9/bin/python
 
-from mpi4py import MPI
+"""
+Version: Python 3.10.9
+
+Description: Optimising parralesisation of an integral calculation equal to pi.
+
+Date: 02/02/2026
+
+Author: Nicholas Young
+"""
+
+from mpi4py import MPI  # pylint: disable=import-error
 
 comm = MPI.COMM_WORLD
-
+rank = comm.Get_rank()
 nproc = comm.Get_size()
-# The first processor is leader, so one fewer available to be a worker
-nworkers = nproc - 1
 
-# samples
 N = 100000000
-delta = 1.0 / N
+DELTA = 1.0 / N
 
-# integral
-I = 0.0
+I_LOCAL = 0.0
 
-def integrand(x):
-  return(4.0 / (1.0 + x*x))
+for ii in range(rank, N, nproc):
+    x = (ii + 0.5) * DELTA
+    I_LOCAL += 4.0 / (1.0 + x*x)
+I_LOCAL *= DELTA
 
-if comm.Get_rank() == 0:
+I_FINAL = comm.reduce(I_LOCAL, op=MPI.SUM, root=0)
 
-  # Leader: choose points to sample function, send to workers and
-  # collect their contributions. Also calculate a sub-set of points.
-
-  for i in range(0,N):
-
-    # decide which rank evaluates this point
-    j = i % nproc
-
-    # mid-point rule
-    x = (i+0.5) * delta
-
-    if j == 0:
-      # so do this locally using the leader machine
-      y = integrand(x) * delta
-    else:
-      # communicate to a worker
-      comm.send(x, dest=j)
-      y = comm.recv(source=j)
-
-    I += y
-
-  # Shut down the workers
-  for i in range(1, nproc):
-    comm.send(-1.0, dest=i)
-
-  print("Integral %.10f" % I)
-
-else:
-
-  # Worker: waiting for something to happen, then stop if sent message
-  # outside the integral limits
-
-  while True:
-
-    x = comm.recv(source=0)
-
-    if x < 0.0:
-      # stop the worker
-      break
+if rank == 0:
+    print(f"Good Code Integral {I_FINAL:.10f}")
 
     else:
       comm.send(integrand(x) * delta, dest=0)
